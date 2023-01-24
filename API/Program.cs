024d8f019dff327162dc58950bdfa91266e57bb9
stynan27 <seamustynan16@gmail.com>
2023-01-24 00:25:56 -0500

    Resolved CORS issues and added SSL

--- API/Program.cs
+++ API/Program.cs
@@ -10,6 +10,8 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
   opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+// Add service to enable Cross Origins
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -18,11 +20,17 @@ var app = builder.Build();
 // i.e opportunity to perform actions on requests 
 // before they reach a destination
 
+// Add middlewear
+
 // middlewear for development only
 // if (app.Environment.IsDevelopment()){}
 
 //app.UseHttpsRedirection(); -> redirects htpp to https (not necessary)
 //app.UseAuthorization(); -> for authentication (later)
+
+// Use added Cross Origin service (above) to allow all headers for any Http Method on the 
+//... cross-origin of our Angular application.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers(); // -> middlewear to map API endpoints to controllers
 
 app.Run();
